a34959362763e71efb195c0bba40b6a7353b16ad
Rafael <leafarmd@gmail.com>
2017-07-28 15:48:06 -0300

    her2

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+
+    var parser = require('rss-parser');
+
+    parser.parseURL('https://www.reddit.com/.rss', function(err, parsed) {
+      console.log(parsed.feed.title);
+      speech = parsed.feed.title;
+      parsed.feed.entries.forEach(function(entry) {
+        console.log(entry.title + ':' + entry.link);
+      })
+    })
+
     return res.json({
         speech: speech,
         displayText: speech,
