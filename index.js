bcc4c65dcce7c63482df5bb71975bd087b533cb0
Rafael <leafarmd@gmail.com>
2017-08-17 16:28:50 -0300

    update

--- index.js
+++ index.js
@@ -12,7 +12,28 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
+  var news = "news";
+  var song = "song";
+  var ubilab = "ubilab";
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+
+    if(speech.indexOf(ubilab) > -1) {
+      return res.json({
+
+          speech: 'Ubilab is a place for academic research which connects theoretical references with their practical application. The lab was created in the Graduate Program of Communications of the Pontifical Catholic University of Rio Grande do Sul (PUCRS) to create a multidisciplinary dialogue to research new perspectives of the Information Society.',
+          displayText: speech,
+          source: 'webhook-echo-sample'
+      });
+    }
+
+    if(speech.indexOf(song) > -1) {
+      return res.json({
+
+          speech: '<speak> heres a song to you <audio src="https://allthingsaudio.wikispaces.com/file/view/Shuffle%20for%20K.M.mp3/139190697/Shuffle%20for%20K.M.mp3">didnt get your MP3 audio file</audio></speak>',
+          displayText: speech,
+          source: 'webhook-echo-sample'
+      });
+    }
 
     var parser = require('rss-parser');
 
@@ -23,6 +44,7 @@ restService.post('/echo', function(req, res) {
         console.log(entry.title + ':' + entry.link);
         speech = entry.title;
         return res.json({
+
             speech: speech,
             displayText: speech,
             source: 'webhook-echo-sample'
