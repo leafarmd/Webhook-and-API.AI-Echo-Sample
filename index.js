77cfd05a822c4d79d6e9e4a16b33b531aa3245ae
Rafael <leafarmd@gmail.com>
2017-08-22 15:26:13 -0300

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ restService.post('/echo', function(req, res) {
   var song = "song";
   var ubilab = "ubilab";
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    var txt = JSON.stringify(req.body);
+    var txt = JSON.stringify(req);
     return res.json({
         speech: txt,
         displayText: speech,
