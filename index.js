c7aefe70c2080c45c0b31233a17b3f9edc1075c8
Rafael <leafarmd@gmail.com>
2017-08-22 14:07:27 -0300

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ restService.post('/echo', function(req, res) {
   var news = "news";
   var song = "song";
   var ubilab = "ubilab";
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters : "Seems like some problem. Speak again."
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.city : "Seems like some problem. Speak again."
 
     return res.json({
         speech: speech,
