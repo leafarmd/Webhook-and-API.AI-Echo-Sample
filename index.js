0d3d49d133302efafe118e9661f837c4e8e47fe4
Rafael <leafarmd@gmail.com>
2017-08-22 15:39:44 -0300

    update

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ restService.post('/echo', function(req, res) {
   var song = "song";
   var ubilab = "ubilab";
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-
+    var txt = JSON.stringify(req.body.originalRequest);
     return res.json({
-        speech: speech,
+        speech: txt,
         displayText: speech,
         source: 'webhook-echo-sample'
     });
