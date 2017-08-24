29bd52fb0c8b97cc876db87724180df204e8bfb0
Rafael <leafarmd@gmail.com>
2017-08-24 15:46:42 -0300

    update

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ restService.post('/echo', function(req, res) {
   var news = "news";
   var song = "song";
   var ubilab = "ubilab";
+  var technology = "technology";
+  var economy = "economy";
+
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
 
     // var txt = JSON.stringify(req.body);
@@ -30,7 +33,7 @@ restService.post('/echo', function(req, res) {
           displayText: speech,
           source: 'webhook-echo-sample'
       });
-    }
+    }else
 
     if(speech.indexOf(song) > -1) {
       return res.json({
@@ -38,8 +41,38 @@ restService.post('/echo', function(req, res) {
           displayText: speech,
           source: 'webhook-echo-sample'
       });
-    }
+    }else
 
+
+    if(speech.indexOf(technology) > -1) {
+      var parser = require('rss-parser');
+      parser.parseURL(' http://g1.globo.com/dynamo/tecnologia/rss2.xml', function(err, parsed) {
+        var speechNews = "";
+        for(var i = 0; i < 4;i++){
+        speechNews = speechNews + "\n" +   parsed.feed.entries[i].title;
+        }
+        return res.json({
+            speech: speechNews,
+            displayText: speech,
+            source: 'webhook-echo-sample'
+        });
+      });
+    }else
+
+    if(speech.indexOf(economy) > -1) {
+      var parser = require('rss-parser');
+      parser.parseURL('  http://g1.globo.com/dynamo/economia/rss2.xml', function(err, parsed) {
+        var speechNews = "";
+        for(var i = 0; i < 4;i++){
+        speechNews = speechNews + "\n" +   parsed.feed.entries[i].title;
+        }
+        return res.json({
+            speech: speechNews,
+            displayText: speech,
+            source: 'webhook-echo-sample'
+        });
+      });
+    }else
 
     if(speech.indexOf(news) > -1) {
       var parser = require('rss-parser');
