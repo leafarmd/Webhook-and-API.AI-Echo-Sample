322df8734fc21d63658468d7544d52f20bbd10ea
Rafael <leafarmd@gmail.com>
2017-08-31 16:12:46 -0300

    update

--- index.js
+++ index.js
@@ -36,6 +36,33 @@ restService.post('/echo', function(req, res) {
   var rj = "rio de janeiro";
   var rs = "rio grande do sul";
 
+  /**
+     * asks for a permission to use the users location
+     * @param assistant the assistant object passed in
+     */
+    function permissionChecker(assistant) {
+        const permission = assistant.SupportedPermissions.DEVICE_PRECISE_LOCATION;
+        assistant.askForPermission('To find the closest bus stop', permission);
+    }
+
+    /**
+     * called when the device gets a callback if they have permission or not
+     * @param assistant the assistant object passed in
+     */
+    function gotPermission(assistant) {
+        if (assistant.isPermissionGranted()) {
+            assistant.ask(JSON.stringify(assistant.getContext("request_permission").parameters));
+
+        } else {
+            assistant.tell("I cannot find when the next bus is coming without your location.");
+        }
+    }
+
+    const actionMap = new Map();
+    actionMap.set('request-location', permissionChecker);
+    actionMap.set('find-location', gotPermission);
+    assistant.handleRequest(actionMap);
+
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
 
     // var txt = JSON.stringify(req.body);
