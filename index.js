9d76d32478ad368465e40aa05288c1361b9ec318
Rafael <leafarmd@gmail.com>
2017-09-08 12:58:56 -0300

    update

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 
 const restService = express();
 process.env.DEBUG = 'actions-on-google:*';
-const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
 const Assistant = require('actions-on-google').ApiAiAssistant;
 restService.use(bodyParser.urlencoded({
     extended: true
@@ -15,11 +14,10 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
 
-  const app = new ActionsSdkApp({request: request, response: response});
   const assistant = new Assistant({ request: req, response: res });
   var message = assistant.getArgument('echoText');
 
-
+  //assistant.ask(JSON.stringify(assistant);
 
   var song = "talk";
   var ubilab = "ubilab";
@@ -69,10 +67,10 @@ restService.post('/echo', function(req, res) {
   //   actionMap.set('find-location', gotPermission);
   //   assistant.handleRequest(actionMap);
 
-  //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
 
-    //  var txt = JSON.stringify(assistant);
-    //  sendResponse(txt);
+    // var txt = JSON.stringify(req.body);
+
     if(message.indexOf(ubilab) > -1) {
       sendResponse('<speak>Ubilab is a place for academic research which connects theoretical references with their practical application. The lab was created in the Graduate Program of Communications of the Pontifical Catholic University of Rio Grande do Sul (PUCRS) to create a multidisciplinary dialogue to research new perspectives of the Information Society.</speak>');
     }else
@@ -149,6 +147,66 @@ restService.post('/echo', function(req, res) {
     }
 
 });
+
+
+
+restService.post('/slack-test', function(req, res) {
+
+    var slack_message = {
+        "text": "Details of JIRA board for Browse and Commerce",
+        "attachments": [{
+            "title": "JIRA Board",
+            "title_link": "http://www.google.com",
+            "color": "#36a64f",
+
+            "fields": [{
+                "title": "Epic Count",
+                "value": "50",
+                "short": "false"
+            }, {
+                "title": "Story Count",
+                "value": "40",
+                "short": "false"
+            }],
+
+            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+        }, {
+            "title": "Story status count",
+            "title_link": "http://www.google.com",
+            "color": "#f49e42",
+
+            "fields": [{
+                "title": "Not started",
+                "value": "50",
+                "short": "false"
+            }, {
+                "title": "Development",
+                "value": "40",
+                "short": "false"
+            }, {
+                "title": "Development",
+                "value": "40",
+                "short": "false"
+            }, {
+                "title": "Development",
+                "value": "40",
+                "short": "false"
+            }]
+        }]
+    }
+
+    return res.json({
+        speech: "speech",
+        displayText: "speech",
+        source: 'webhook-echo-sample',
+        data: {
+            "slack": slack_message
+        }
+    });
+});
+
+
+
 
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
