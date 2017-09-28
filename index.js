b8027e2f3b1c2c60b698fc0889f87c5d54d25347
Rafael <leafarmd@gmail.com>
2017-09-28 12:54:27 -0300

    update

--- index.js
+++ index.js
@@ -37,28 +37,13 @@ restService.post('/echo', function(req, res) {
 
     if(message.indexOf(ubilab) > -1) {
       //sendResponse("Ubilab is a place for academic research which connects theoretical references with their practical application. The lab was created in the Graduate Program of Communications of the Pontifical Catholic University of Rio Grande do Sul (PUCRS) to create a multidisciplinary dialogue to research new perspectives of the Information Society.");
-
-          let preciseLocationPermission = assistant.SupportedPermissions.DEVICE_PRECISE_LOCATION;
-          let namePermission = assistant.SupportedPermissions.NAME;
-          assistant.askForPermissions('To address you by name and know your location',[namePermission,preciseLocationPermission]);
-          if(message.indexOf(song) > -1) {
-            if (assistant.isPermissionGranted()) {
-              let displayName = assistant.getUserName().displayName;
-              let deviceCoordinates = assistant.getDeviceLocation().coordinates;
-              //assistant.sendResponse(assistant.getDeviceLocation().coordinates.longitude);
-              assistant.sendResponse(displayName);
-            }
+      let preciseLocationPermission = assistant.SupportedPermissions.DEVICE_PRECISE_LOCATION;
+      assistant.askForPermissions('To address you by name and know your location',[preciseLocationPermission]);
 
     }else
 
     if(message.indexOf(song) > -1) {
-      if (assistant.isPermissionGranted()) {
-        let displayName = assistant.getUserName().displayName;
-        let deviceCoordinates = assistant.getDeviceLocation().coordinates;
-        //assistant.sendResponse(assistant.getDeviceLocation().coordinates.longitude);
-        assistant.sendResponse(displayName);
-      }
-
+      assistant.sendResponse(assistant.getDeviceLocation().coordinates.latitude);
       //sendResponse('<speak> playing audio news <audio src="https://leafarmd.000webhostapp.com/news.mp3"></audio></speak>')
     }else
 
