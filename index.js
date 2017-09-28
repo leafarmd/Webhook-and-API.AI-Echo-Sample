55b501956c3fd26a201469bca9f656dc03676c99
Rafael <leafarmd@gmail.com>
2017-09-28 11:16:09 -0300

    update

--- index.js
+++ index.js
@@ -34,16 +34,17 @@ restService.post('/echo', function(req, res) {
   var rj = "rio de janeiro";
   var rs = "rio grande do sul";
 
-    let preciseLocationPermission = app.SupportedPermissions.DEVICE_PRECISE_LOCATION;
-    assistant.askForPermissions('To address you by name and know your location',[namePermission, preciseLocationPermission]);
-
 
     if(message.indexOf(ubilab) > -1) {
       sendResponse("Ubilab is a place for academic research which connects theoretical references with their practical application. The lab was created in the Graduate Program of Communications of the Pontifical Catholic University of Rio Grande do Sul (PUCRS) to create a multidisciplinary dialogue to research new perspectives of the Information Society.");
     }else
 
     if(message.indexOf(song) > -1) {
-      sendResponse('<speak> playing audio news <audio src="https://leafarmd.000webhostapp.com/news.mp3"></audio></speak>')
+
+          let preciseLocationPermission = app.SupportedPermissions.DEVICE_PRECISE_LOCATION;
+          assistant.askForPermissions('To address you by name and know your location',[namePermission, preciseLocationPermission]);
+
+      //sendResponse('<speak> playing audio news <audio src="https://leafarmd.000webhostapp.com/news.mp3"></audio></speak>')
     }else
 
 
